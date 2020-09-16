f1fcb0dcb36dcadd47e9c9368596d1a4922486d4
Kimberly Flores <kimberlyflores@Kimberlys-MacBook-Pro.local>
2020-09-16 00:30:09 -0500

    making initial api call

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b729df775d22e20347e866a364729fa"
+weather_api_key = "b729df775d22e20347e866a364729faa"
 
 # Google API Key
 g_key = "AIzaSyA4alsVXFheadxUjEnmZgacb7en3OMxhgA"
