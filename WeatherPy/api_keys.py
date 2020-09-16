65dffc1658a668b36a6b501d4256517732650c92
Kimberly Flores <kimberlyflores@Kimberlys-MacBook-Pro.local>
2020-09-15 23:47:05 -0500

    uploading initial city list

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b729df775d22e20347e866a364729fa"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA4alsVXFheadxUjEnmZgacb7en3OMxhgA"
